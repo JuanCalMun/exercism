c6d1d0d5ff70c6177bb8938b1baf787b56319bd3
JuanCalMun <JuanCalMun@github.com>
2021-08-22 16:02:17 +0200

    updates

--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,12 +1,22 @@
+import scala.collection.{SortedMap, mutable}
+import scala.math.Numeric.IntIsIntegral
+
 class School {
   type DB = Map[Int, Seq[String]]
+  var database: DB = Map()
 
-  def add(name: String, g: Int) = ???
+  def add(name: String, g: Int) = {
+    if (database.contains(g))
+      database += database(g) :+ name
+    else database += (g -> Seq(name))
+  }
 
-  def db: DB = ???
+  def db: DB = database
 
-  def grade(g: Int): Seq[String] = ???
+  def grade(g: Int): Seq[String] =
+    if (database.contains(g)) database(g)
+    else Seq()
 
-  def sorted: DB = ???
+  def sorted: DB = SortedMap(database.toSeq:_*).toMap
 }
 
