5e278fdea9b1e5350266bea415c0726bcb8441d4
JuanCalMun <JuanCalMun@github.com>
2021-09-05 17:50:52 +0200

    Improved hammling

---
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -0,0 +1,8 @@
+
+
+object Hamming {
+  def distance(s1: String, s2: String) = (s1, s2) match {
+    case (s1, s2) if s1.length != s2.length => None
+    case _ => Some(s1.zip(s2).count(x => x._1 != x._2))
+  }
+}
